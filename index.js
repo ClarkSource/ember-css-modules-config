46607ca1424b91f004f1dbf4b5a20e0ba9f5c8e8
Jan Buschtöns <jan.buschtoens@clark.de>
2019-07-25 11:42:49 +0200

    chore: make `ember s` work again

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const ClarkPlugin = require('./lib/clark-plugin');
 module.exports = {
   name: require('./package').name,
 
+  shouldIncludeChildAddon(childAddon) {
+    if (childAddon.name === 'ember-css-modules') {
+      return false;
+    }
+    return this._super.shouldIncludeChildAddon.call(this, childAddon);
+  },
+
   createCssModulesPlugin(parent) {
     return new ClarkPlugin(parent);
   }
