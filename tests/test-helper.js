2082f646d06cd8f1d28f9e054f21c4903cdcbdb2
Jan Buschtöns <jan.buschtoens@clark.de>
2021-07-30 03:13:05 +0200

    style: fix `eslint`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,10 @@
-import Application from '../app';
-import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
+
+import Application from '../app';
+import config from '../config/environment';
 
 setup(QUnit.assert);
 
